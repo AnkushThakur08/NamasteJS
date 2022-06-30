0bd8d9935b2c6e7c6b9d7fff42ddf2acddbdfa8e
Ankush Thakur <atpara99@gmail.com>
2022-06-30 12:51:32 +0530

    Namaste JS Day-1

---
+++ 01executionContext.js
@@ -0,0 +1,15 @@
+var n = 2;
+
+function square(num) {
+  ans = num * num;
+  return ans;
+}
+
+var ankush = () => {
+  return {
+    //
+  };
+};
+
+var square2 = square(n);
+var square4 = square(4);
---
+++ 02hosting.js
@@ -0,0 +1,16 @@
+console.log(n);
+
+var square2 = square(n);
+var square4 = square(4);
+  
+
+function square(num) {
+  ans = num * num;
+  console.log(ans);
+  return ans;
+}
+
+var ankush = () => {
+  var a = 20;
+  console.log(a);
+};
