54806757e93499a982e42239e65fd6b05297c625
Ankush Thakur <atpara99@gmail.com>
2024-03-17 22:34:43 +0530

    Javascript revised

--- revision2.js
+++ revision2.js
@@ -56,10 +56,109 @@ function getNumber() {
 getNumber();
  */
 
-/* SetTimeOut */
+/*TODO: SetTimeOut */
 
-// for (let i = 1; i <= 5; i++) {
-//   setTimeout(() => {
-//     console.log(i);
-//   }, [i * 1000]);
-// }
+/* 
+for (let i = 1; i <= 5; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, [i * 1000]);
+}
+
+for (var i = 1; i <= 5; i++) {
+  function close(x) {
+    setTimeout(() => {
+      console.log(x);
+    }, [x * 1000]);
+  }
+
+  close(i);
+}
+ */
+
+/* TODO: block Scope */
+
+/* let b = 100;
+
+{
+  var a = 10;
+  let b = 20;
+  const c = 30;
+  console.log(a, b, c);
+}
+
+console.log(b);
+ */
+
+/* TODO: Closures */
+/* 
+function getName() {
+  var myName = "Ankush Thakur";
+
+  function printName() {
+    console.log(myName);
+  }
+
+  printName();
+}
+
+getName();
+
+function getName() {
+  var myName = "Ankush Thakur";
+
+  function printName() {
+    console.log(myName);
+  }
+
+  return printName;
+}
+
+const result = getName();
+
+result();
+ */
+
+/* TODO: Data Hiding and Encaplusation */
+
+/* 
+function counter() {
+  var count = 0;
+  function incrementCounter() {
+    count++;
+    console.log(count);
+  }
+  return incrementCounter;
+}
+
+
+counter()();
+var result = counter();
+result();
+
+
+// THIS IS NOT A GOOD WAY, GOOD WAY IS WITH CONSTRUCTOR
+
+
+function Counter() {
+    var count = 0;
+    this.incrementCounter = function () {
+      count++;
+      console.log(count);
+    };
+  
+  
+    this.decrementCounter = function () {
+      count--;
+      console.log(count);
+    };
+  }
+  
+  
+  var counter1 = new Counter();
+  
+  
+  counter1.incrementCounter();
+  counter1.incrementCounter();
+  counter1.decrementCounter();
+   */
