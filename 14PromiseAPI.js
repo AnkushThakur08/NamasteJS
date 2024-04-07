0cdb682902a3c54abf22289d222c7942617f28fe
Ankush Thakur <atpara99@gmail.com>
2024-04-07 21:03:34 +0530

    promise api

---
+++ 14PromiseAPI.js
@@ -0,0 +1,32 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("P1 Resolve");
+  }, 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("P2 Resolve");
+    reject("P2 FAIL");
+  }, 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("P3 Resolve");
+    reject("P3 Reject");
+  });
+}, 2000);
+
+// const result = Promise.all([p1, p2, p3]);
+// const result = Promise.allSettled([p1, p2, p3]);
+// const result = Promise.race([p1, p2, p3]);
+const result = Promise.any([p1, p2, p3]);
+
+result
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
